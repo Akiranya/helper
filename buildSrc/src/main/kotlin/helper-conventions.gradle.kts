1a52e9c4c9fe2c6e47daae759ce1144bfdbc6f62
Akiranya <porridgen@gmail.com>
2024-01-11 01:55:43 +0800

    add deploy task

--- buildSrc/src/main/kotlin/helper-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/helper-conventions.gradle.kts
@@ -166,4 +166,34 @@ tasks {
     javadocJar {
         enabled = false
     }
+
+    ////// deploy //////
+
+    val inputJarPath = lazy { shadowJar.get().archiveFile.get().asFile.absolutePath }
+    val finalJarName = lazy { "${ext.get("name")}-${project.version}.jar" }
+    val finalJarPath = lazy { layout.buildDirectory.file(finalJarName.value).get().asFile.absolutePath }
+
+    register<Copy>("copyJar") {
+        group = "mewcraft"
+        from(inputJarPath.value)
+        into(layout.buildDirectory)
+        rename("(?i)${project.name}.*\\.jar", finalJarName.value)
+    }
+    register<Task>("deployJar") {
+        group = "mewcraft"
+        doLast {
+            exec {
+                commandLine("rsync", finalJarPath.value, "dev:data/dev/jar")
+            }
+        }
+    }
+    register<Task>("deployJarFresh") {
+        group = "mewcraft"
+        dependsOn(build)
+        finalizedBy(named("deployJar"))
+    }
+
+    build {
+        finalizedBy("copyJar")
+    }
 }
