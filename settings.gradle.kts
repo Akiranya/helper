5e862c68f140371c2035a3c4fc82a11dd0f3fc05
Akiranya <porridgen@gmail.com>
2024-01-11 14:12:42 +0800

    temporarily remove helper-mongo project

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,9 +1,9 @@
 rootProject.name = "helper-parent"
 
 include(":helper")
-// include(":helper-lilypad") // Too old to be used in production env
+// include(":helper-lilypad") // too old
 include(":helper-sql")
 include(":helper-redis")
-include(":helper-mongo")
+// include(":helper-mongo") // too old, update it
 include(":helper-profiles")
-// include(":helper-js") // Too old to be used in production env
+// include(":helper-js") // too old
