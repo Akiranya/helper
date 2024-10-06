ca2350b6cfffa634c68eca0652347fb9998f2b72
Akiranya <porridgen@gmail.com>
2024-10-06 16:55:15 +0800

    completely remove helper-js & helper-lilypad

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,9 +1,15 @@
+dependencyResolutionManagement {
+    versionCatalogs {
+        create("local") {
+            from(files("gradle/local.versions.toml"))
+        }
+    }
+}
+
 rootProject.name = "helper-parent"
 
 include(":helper")
-// include(":helper-lilypad") // too old
 include(":helper-sql")
 include(":helper-redis")
 include(":helper-mongo") // TODO too old, update it
 include(":helper-profiles")
-// include(":helper-js") // too old
